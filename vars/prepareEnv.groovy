fdd0c1e1dc601ba3dac99d6773c99b8badc37019
shirtzo <shirtzo123@gmail.com>
2025-02-23 15:19:20 +0000

    checking

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -1,15 +1,4 @@
 def call() {
-    // Checkout the repository and get the SCM information
-    def scmVars = checkout([
-        $class: 'GitSCM',
-        branches: [[name: '*/main']],  // Adjust the branch name accordingly
-        extensions: [],
-        userRemoteConfigs: [[url: 'https://github.com/shirtzo/TaseExam.git']]
-    ])
-
-    // // Set the retrieved values as environment variables
-    // env.GIT_COMMIT = scmVars.GIT_COMMIT
-    // env.GIT_COMMITTER_EMAIL = scmVars.GIT_COMMITTER_EMAIL
-
-    echo "hello"
+    echo "Commit hash: ${env.GIT_COMMIT}"
+    echo "Committer name: ${env.GIT_COMMITTER_NAME}"
 }
