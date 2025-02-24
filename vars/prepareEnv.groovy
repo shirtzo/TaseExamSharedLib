2a108b1603e6aa626d2c2b36515d370b3272e06b
shirtzo <shirtzo123@gmail.com>
2025-02-24 16:28:12 +0000

    changes methos

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -2,7 +2,7 @@ def call() {
     def committer = sh(script: "git show -s --format=\"%cn\" ${GIT_COMMIT}", returnStdout: true).trim()
     
     return [
-        codeCommiter: env.GIT_COMMITTER_NAME, 
+        codeCommiter: commiter, 
         commitHash: env.GIT_COMMIT, 
         gitUrl: env.GIT_URL
     ]
