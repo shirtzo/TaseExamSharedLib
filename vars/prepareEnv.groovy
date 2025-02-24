551aeb2c2c4faa99ad663d8f26a8aca838090203
shirtzo <shirtzo123@gmail.com>
2025-02-24 16:53:57 +0000

    changed to email

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -1,8 +1,8 @@
 def call() {
     def committerEmail = sh (script: 'git --no-pager show -s --format=\'%ae\'', returnStdout: true).trim()
-    
+
     return [
-        codeCommiter: committer, 
+        codeCommiter: committerEmail, 
         commitHash: env.GIT_COMMIT, 
         gitUrl: env.GIT_URL
     ]
