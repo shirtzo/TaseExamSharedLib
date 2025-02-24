68b41f077444aa59b24ed7ec3a06440bafa06090
shirtzo <shirtzo123@gmail.com>
2025-02-24 18:50:32 +0000

    hardcoded hash

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -12,7 +12,7 @@ def call() {
     def committerEmail = sh(script: "git --no-pager show -s --format='%ae' ${env.GIT_COMMIT}", returnStdout: true).trim()
 
     return [
-        codeCommiter: env.GIT_COMMITTER_NAME,
+        codeCommiter: env.GIT_COMMITTER,
         codeCommiterEmail: committerEmail,
         commitHash: env.GIT_COMMIT, 
         gitUrl: env.GIT_URL
