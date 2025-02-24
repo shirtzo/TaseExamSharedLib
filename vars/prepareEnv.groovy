534179cab08692aa936639883a90766f5a2ff0a9
shirtzo <shirtzo123@gmail.com>
2025-02-24 17:25:14 +0000

    changed to specific commit

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -1,5 +1,6 @@
 def call() {
-    def committerName = sh(script: "git --no-pager show -s --format='%cn' ${env.GIT_COMMIT}", returnStdout: true).trim()
+    def gitCommitHash = sh(script: "git log -n 1 --pretty=format:'%H'", returnStdout: true)
+    def committerName = sh(script: "git --no-pager show -s --format='%cn' ${gitCommitHash}", returnStdout: true).trim()
 
     return [
         codeCommiter: committerName, 
