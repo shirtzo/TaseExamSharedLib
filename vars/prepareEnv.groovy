3a84faf2cef9f0547c5e360f7348707c0f1cdf9e
shirtzo <shirtzo123@gmail.com>
2025-02-24 18:33:06 +0000

    hardcoded hash

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -1,9 +1,19 @@
 def call() {
-    def gitCommitHash = 'e8333ae8eeb4dc6f8b92e59c809187730ae9167b'
-    def committerName = sh(script: "git --no-pager show -s --format='%cn' ${gitCommitHash}", returnStdout: true).trim()
+    def scmVars = checkout([
+        $class: 'GitSCM',
+        branches: [[name: '*/main']],
+        userRemoteConfigs: [[url: 'https://github.com/your/repo.git']],
+        extensions: [[$class: 'CloneOption', depth: 0, noTags: false]]
+      ])
+
+    
+    env.GIT_COMMIT = scmVars.GIT_COMMIT
+    def committerName = sh(script: "git --no-pager show -s --format='%cn' ${env.GIT_COMMIT}", returnStdout: true).trim()
+    def committerEmail = sh(script: "git --no-pager show -s --format='%ae' ${env.GIT_COMMIT}", returnStdout: true).trim()
 
     return [
-        codeCommiter: committerName, 
+        codeCommiter: committerName,
+        codeCommiterEmail: committerEmail,
         commitHash: env.GIT_COMMIT, 
         gitUrl: env.GIT_URL
     ]
